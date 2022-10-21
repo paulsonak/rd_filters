8242d369851694b57755d7a9f81cf4f0f73de058
Amanda Paulson <akpaulson547@gmail.com>
2022-10-21 15:36:33 -0700

    add molecule visualisation functions and ability to use in jupyter

--- rd_filters/rd_filters.py
+++ rd_filters/rd_filters.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
-
+"""
+Adapted from Pat Walters to be used with jupyter
+Added several functions to visualize molecules and filters
+AKP 10/21/2022
+"""
+#################################################
 import sys
 from rdkit import Chem
 from rdkit.Chem.Descriptors import MolWt, MolLogP, NumHDonors, NumHAcceptors, TPSA
@@ -26,6 +31,12 @@ Options:
 --out TEMPLATE_FILE parameter template file name
 """
 
+def get_rules_files(rd_filter_data_path):
+    rules={}
+    for file in os.listdir(rd_filter_data_path):
+        if '.json' in file:
+            rules[file.replace('.json','')]=file
+    return rules
 
 def read_rules(rules_file_name):
     """
@@ -152,6 +163,184 @@ class RDFilters:
             if len(mol.GetSubstructMatches(patt)) > max_val:
                 return [smiles, name] + [desc + " > %d" % (max_val)] + desc_list
         return [smiles, name] + ["OK"] + desc_list
+
+def filter_mols(cmd_input, verbose=True):
+    """ A function to replicate the command line in a jupyter notebook.
+    :param cmd_input: input dict of {
+    (template|filter):boolean, 
+    out: a file location for creating a template,
+    in: an input .smi file, 
+    data_path: a path to the directory of alerts and template files, 
+    rules: label for which template file to use, 
+    prefix: an indicator of where to save filtering info and what to call it, 
+    num_cores: number of cores to use with default all cores, 
+    alerts: a file name indicating which alerts to use instead of a template
+    }
+    :return: saves template to out or filter info to prefix
+    """
+    alert_file_name = cmd_input.get("alerts") or pkg_resources.resource_filename('rd_filters', "data/alert_collection.csv")
+    rf = RDFilters(alert_file_name)
+
+    if cmd_input.get("template"):
+        template_output_file = cmd_input.get("out")
+        default_rule_template(rf.get_alert_sets(), template_output_file)
+
+    elif cmd_input.get("filter"):
+        input_file_name = cmd_input.get("in")
+        data_path=cmd_input.get("data_path") or pkg_resources.resource_filename('rd_filters', "data")
+        rules_files=get_rules_files(data_path)
+        rules_file_name = os.path.join(data_path, rules_files[cmd_input.get("rules")]) or pkg_resources.resource_filename('rd_filters', "data/rules_all.json")
+        rules_file_path = get_config_file(rules_file_name, "FILTER_RULES_DATA")
+        prefix_name = cmd_input.get("prefix")
+        num_cores = cmd_input.get("num_cores") or mp.cpu_count()
+        num_cores = int(num_cores)
+
+        if verbose:
+            print("using %d cores" % num_cores, file=sys.stderr)
+        start_time = time.time()
+        p = Pool(num_cores)
+        input_data = [x.split() for x in open(input_file_name)]
+        input_data = [x for x in input_data if len(x) == 2]
+        rule_dict = read_rules(rules_file_path)
+
+        rule_list = [x.replace("Rule_", "") for x in rule_dict.keys() if x.startswith("Rule") and rule_dict[x]]
+        rule_str = " and ".join(rule_list)
+        if verbose:
+            print(f"Using alerts from {rule_str}", file=sys.stderr)
+        rf.build_rule_list(rule_list)
+        res = list(p.map(rf.evaluate, input_data))
+        df = pd.DataFrame(res, columns=["SMILES", "NAME", "FILTER", "MW", "LogP", "HBD", "HBA", "TPSA", "Rot"])
+        df_ok = df[
+            (df.FILTER == "OK") &
+            df.MW.between(*rule_dict["MW"]) &
+            df.LogP.between(*rule_dict["LogP"]) &
+            df.HBD.between(*rule_dict["HBD"]) &
+            df.HBA.between(*rule_dict["HBA"]) &
+            df.TPSA.between(*rule_dict["TPSA"]) &
+            df.Rot.between(*rule_dict["Rot"])
+            ]
+        output_smiles_file = prefix_name + ".smi"
+        output_csv_file = prefix_name + ".csv"
+        df_ok[["SMILES", "NAME"]].to_csv(f"{output_smiles_file}", sep=" ", index=False, header=False)
+        if verbose:
+            print(f"Wrote SMILES for molecules passing filters to {output_smiles_file}", file=sys.stderr)
+        df.to_csv(f"{prefix_name}.csv", index=False)
+        if verbose:
+            print(f"Wrote detailed data to {output_csv_file}", file=sys.stderr)
+
+        num_input_rows = df.shape[0]
+        num_output_rows = df_ok.shape[0]
+        fraction_passed = "%.1f" % (num_output_rows / num_input_rows * 100.0)
+        if verbose:
+            print(f"{num_output_rows} of {num_input_rows} passed filters {fraction_passed}%", file=sys.stderr)
+        elapsed_time = "%.2f" % (time.time() - start_time)
+        if verbose:
+            print(f"Elapsed time {elapsed_time} seconds", file=sys.stderr)
+        
+        
+##################################################################
+##################################################################
+
+from atomsci.ddm.utils import rdkit_easy
+
+# This function enumerates and scores tautomers for the molecule passed as parameter, puts them in a `pandas.DataFrame` and sorts it by decreasing score.
+# from https://gist.github.com/ptosco/20b06985cd8830d5e549165f6b9fc969
+def get_tautomer_dataframe(mol):
+    te = rdMolStandardize.TautomerEnumerator()
+    te.SetMaxTransforms(2000)
+    res = te.Enumerate(mol)
+    df = pd.DataFrame({"TautSmiles": [Chem.MolToSmiles(t) for t in res], "TautScore": [te.ScoreTautomer(t) for t in res]})
+    PandasTools.AddMoleculeColumnToFrame(df, "TautSmiles")
+    df.sort_values(by=["TautScore", "TautSmiles"], ascending=[False, True], inplace=True)
+    return df
+
+# function to align two molecule images based on RDKit cookbook
+def align_mol(template, mol2):
+    AllChem.Compute2DCoords(template)
+    mcs = FindMCS([template,mol2], 
+                  completeRingsOnly=True, 
+                  ringMatchesRingOnly=True, 
+                  timeout=10)
+    patt = Chem.MolFromSmarts(mcs.smartsString)
+    template_match = template.GetSubstructMatch(patt)
+    query_match = mol2.GetSubstructMatch(patt)
+    AllChem.Compute2DCoords(mol2)
+    rms = AllChem.AlignMol(mol2, template, atomMap=list(zip(query_match,template_match)))
+    
+# draw original structure, best tautomer, and a corrected/"best" structure if exists
+def draw_structures(smiles_dict, corr_smiles_dict, save=False, outdir='./'):
+    for root in smiles_dict:
+        smi = smiles_dict[root]
+        mol = Chem.MolFromSmiles(smi)
+        canmol=rdMolStandardize.CanonicalTautomer(mol)
+        if root in corr_smiles_dict.keys():
+            corsmi=corr_smiles_dict[root]
+            cormol=Chem.MolFromSmiles(corsmi)
+            align_mol(canmol, cormol)
+        else:
+            cormol=Chem.MolFromSmiles('[Na+]')
+        mols=[mol,canmol,cormol]
+        legends=[root,'Canonical tautomer','Jeff corrected']
+        svg=Draw.MolsToGridImage(mols, legends=legends, molsPerRow=3, subImgSize=(300,300),highlightAtomLists=None, highlightBondLists=None, useSVG=True)
+        display(svg)
+        if save:
+            filename=os.path.join(outdir, f'{root}.svg')
+            with open(filename, 'w') as f:
+                    f.write(svg.data)
+
+def draw_filters(filterdict):
+    for label, smarts in filterdict.items():
+        mol=Chem.MolFromSmarts(smarts)
+        print(label)
+        display(mol)
+
+def get_fail_smarts(filter_string, filters):
+    filter_string=filter_string.split(' > ')[0]
+    smarts_list =  filters[filters.description==filter_string].smarts.tolist()
+    return smarts_list
+
+# https://stackoverflow.com/questions/69735586/how-to-highlight-the-substructure-of-a-molecule-with-thick-red-lines-in-rdkit-as
+def increase_resolution(mol, substructure, size=(400, 400)):
+    # mol = deepcopy(mol)
+    # substructure = deepcopy(substructure)
+    drawer = Chem.Draw.rdMolDraw2D.MolDraw2DSVG(size[0], size[1])
+    # highlightAtoms expects only one tuple, not tuple of tuples. So it needs to be merged into a single tuple
+    matches = sum(mol.GetSubstructMatches(substructure), ())
+    drawer.DrawMolecule(mol, highlightAtoms=matches)
+    drawer.FinishDrawing()
+    svg = drawer.GetDrawingText()
+    svg = svg.replace('xmlns:svg','xmlns')
+    return svg.replace('svg:','')
+
+def draw_filter_on_mol(smiles, filter_string, filters_df):
+    m = Chem.MolFromSmiles(smiles)
+    smarts=get_fail_smarts(filter_string, filters_df)
+    if len(smarts)==0:
+        smarts=['']
+    substructure = Chem.MolFromSmarts(smarts[0])
+    # m=increase_resolution(m, substructure)
+    m.GetSubstructMatches(substructure)
+    return m
+
+def draw_filter_on_mol_df(smiles_col, filter_col, df, filters_df,  mol_col='FilteredMol'):
+    drawn_mols=[]
+    for smiles, filter_string in zip(df[smiles_col], df[filter_col]):
+        mol = draw_filter_on_mol(smiles, filter_string, filters_df)
+        drawn_mols.append(mol)
+    df[mol_col]=drawn_mols
+    
+def add_filter_col_to_df(filter_col, df, filters_df, mol_col='FilterMol'):
+    filtmols=[]
+    for filter_string in df[filter_col]:
+        smarts=get_fail_smarts(filter_string, filters_df)
+        if len(smarts)==0:
+            smarts=['']
+        filtmols.append(Chem.MolFromSmarts(smarts[0]))
+    df[mol_col]=filtmols
+
+
+##################################################################
+##################################################################
 
 
 def main():
